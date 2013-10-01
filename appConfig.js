6e2c6199133606af539fa94d9873b3c5cdc0fb5d
jeroendedauw <jeroendedauw@gmail.com>
2013-10-01 14:53:05 +0200

    Improve appConfig immutability

--- appConfig.js
+++ appConfig.js
@@ -5,14 +5,12 @@ var extend = require('extend');
 
 var baseDir = process.cwd();
 
-var config = {
-	DEFAULT_BUILD: 'ExampleConfig',
-
-	BUILD_CONFIG_DIR: path.resolve(baseDir, 'build_config'),
-	BUILD_DIR: path.resolve(baseDir, 'build'),
-	COMPOSER_COMMAND: 'php ' + path.resolve(baseDir, 'bin/composer.phar')
-};
-
 module.exports = function() {
-	return extend({}, config);
+	return {
+		DEFAULT_BUILD: 'ExampleConfig',
+
+		BUILD_CONFIG_DIR: path.resolve(baseDir, 'build_config'),
+		BUILD_DIR: path.resolve(baseDir, 'build'),
+		COMPOSER_COMMAND: 'php ' + path.resolve(baseDir, 'bin/composer.phar')
+	};
 };
