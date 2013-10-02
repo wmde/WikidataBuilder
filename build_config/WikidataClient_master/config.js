7c70a0ebf273edc0901fe1cba9e4c26df8c60b9d
jeroendedauw <jeroendedauw@gmail.com>
2013-10-02 16:38:50 +0200

    Get rid of timestamp build dirs

--- build_config/WikidataClient_master/config.js
+++ build_config/WikidataClient_master/config.js
@@ -2,5 +2,5 @@
 
 module.exports = {
 	NAME_OF_TOP_DIR: 'WikidataClient',
-	BUILD_DIR: 'WikidataClient_master/' + Math.round((new Date()).getTime() / 1000 ).toString()
+	BUILD_DIR: 'WikidataClient_master'
 };
--- build_config/WikidataRepo_master/config.js
+++ build_config/WikidataRepo_master/config.js
@@ -2,5 +2,5 @@
 
 module.exports = {
 	NAME_OF_TOP_DIR: 'WikidataRepo',
-	BUILD_DIR: 'WikidataRepo_master/' + Math.round((new Date()).getTime() / 1000 ).toString()
+	BUILD_DIR: 'WikidataRepo_master'
 };
