8f6eb794c14c7100fae260c58aeb9844fd21b8f2
jeroendedauw <jeroendedauw@gmail.com>
2013-10-02 17:21:00 +0200

    Load example config for wikibase repo and client

--- build_config/WikidataClient_master/build_resources/WikidataClient.php
+++ build_config/WikidataClient_master/build_resources/WikidataClient.php
@@ -5,3 +5,5 @@ if ( !is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
 }
 
 include_once( __DIR__ . '/vendor/autoload.php' );
+
+include_once( __DIR__ . '/vendor/wikibase/wikibase/client/ExampleSettings.php' );
